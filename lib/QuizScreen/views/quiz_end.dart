42ecb61f48eafed1ac0ad2b45e7a997ce1f93160
Jeremiah Ekanem <jeremyessien@gmail.com>
2022-11-24 11:03:46 +0100

    Created  quiz_rules.dart

---
+++ lib/QuizScreen/views/quiz_end.dart
@@ -0,0 +1,15 @@
+import '../../../app.dart';
+
+class QuizEnd extends ConsumerStatefulWidget {
+  const QuizEnd({Key? key}) : super(key: key);
+
+  @override
+  State<QuizEnd> createState() => _QuizEndState();
+}
+
+class _QuizEndState extends State<QuizEnd> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
